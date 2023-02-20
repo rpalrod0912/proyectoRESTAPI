d300b20575d1b426db0f574312d8202f7ce2195d
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-20 18:59:57 +0100

    Cors solucionado

--- src/main/java/com/example/proyectorestapi/ProyectoRestapiApplication.java
+++ src/main/java/com/example/proyectorestapi/ProyectoRestapiApplication.java
@@ -1,14 +1,32 @@
 package com.example.proyectorestapi;
 
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication(exclude ={ SecurityAutoConfiguration.class } )
 public class ProyectoRestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoRestapiApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+
 
 }
